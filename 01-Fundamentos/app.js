182fe2741be0acc9fa75165b2ce8c2a222d11fb1
edgardo <nergumero2017@gmail.com>
2020-08-05 18:13:24 -0300

    Variables y tipos de datos

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,1 +1,18 @@
 console.log("Hola a todos!!")
+
+var primerNombre = 'Grover';
+console.log(primerNombre);
+
+var edad = 34;
+var sueldo = 1809.99;
+
+console.log(edad);
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+
+tieneTrabajo = null;
+console.log(tieneTrabajo);
