1c587c4126340f1c166183f4b203e3ff2dad357b
edgardo <nergumero2017@gmail.com>
2020-08-05 18:51:19 -0300

    operadores

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,4 +1,4 @@
-console.log("Hola a todos!!")
+/*console.log("Hola a todos!!")
 
 var primerNombre = 'Grover';
 console.log(primerNombre);
@@ -16,3 +16,86 @@ console.log(puestoDeTrabajo);
 
 tieneTrabajo = null;
 console.log(tieneTrabajo);
+
+// no se puede utilizar variables reservadas
+// var function= 1212
+
+// tampoco se puede utilizar numero o simbolo al comienzo, salvo signo $ y _
+
+var primerNombre,edad, sexo, esSoltero;
+primerNombre = 'Grover';
+edad = 34;
+sexo = 'M';
+esSoltero = false;
+// Hace una conversion implicita de datos por estar dentro de un string
+console.log('El nombre es:' + primerNombre + ', y su edad es:' + edad + ', es soltero?' + esSoltero);
+
+// mutación de variables: permite cambiar el tipo de datos a una misma variable
+edad = 'veinte'
+
+
+//Operadores
+
+var edadGrover, edadPablo, diferenciaEdad, sumaEdades, yearActual, yearGrover, yearPablo;
+edadGrover = 34;
+edadPablo = 28;
+yearActual = 2020
+diferenciaEdad = edadGrover - edadPablo;
+sumaEdades = edadGrover + edadGrover;
+yearGrover = yearActual - edadGrover;
+yearPablo = yearActual - edadPablo;
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log("Año en que nació Grover" + yearGrover);
+console.log("Año en que nació Pablo" + yearPablo);
+
+//Operadores lógicos
+
+var mayorGrover = edadGrover > edadPablo;
+console.log(mayorGrover);
+
+//Operador typeof: me da el tipo de dato de una variable
+console.log(typeof edadGrover);
+console.log(typeof edadPablo);
+console.log(mayorGrover);
+console.log(typeof 'Grover es mayor que Pablo')
+*/
+
+
+// Operadores Unarios Aritméticos
+// ++ Incremento
+//-- Decremento
+
+var edadCarmen = 18;
+var edadMaria = 24;
+
+++edadCarmen;
+edadCarmen++;
+console.log(edadCarmen);
+
+// aca muestra primero el valor y despues incrementa
+console.log(edadCarmen++);
+console.log(edadCarmen)
+
+
+//Operadores de asignación
+//=
+
+var a = 5;
+var b= 18;
+
+a = a + b;
+//asignación de adición
+a += b;
+a -= b;
+a *= b;
+a /= b;
+var c;
+c = a% b; // devuelve el residuo de una división;
+
+
+
+
+
+
+
